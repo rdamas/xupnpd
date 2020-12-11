66cd9da3f4ae673dbc09b0dbbf2d834a50232f26
Robert Damas <rdamas@byom.de>
2020-12-11 15:33:54 +0100

    Add a install target in Makefile

--- src/rules.mk
+++ src/rules.mk
@@ -3,9 +3,11 @@ EXTRA_CFLAGS  = -fno-exceptions -fno-rtti -Os -I$(LUA) $(MY_CFLAGS) -DWITH_OPENS
 OBJS    = main.o soap.o mem.o mcast.o luaxlib.o luaxcore.o luajson.o luajson_parser.o md5c.o
 LIBS    = $(LUA)/liblua.a
 
+INSTALL = install
+PREFIX  = /usr/local
+
 all: $(LIBS) $(OBJS)
 	$(CC) $(CFLAGS) -o $(TARGET_DIR)/xupnpd $(OBJS) $(LIBS) -ldl -lm -lcrypto -lssl
-	$(STRIP) $(TARGET_DIR)/xupnpd
 
 $(LUA)/liblua.a:
 	$(MAKE) -C $(LUA) a CC="$(CC)" MYCFLAGS="-DLUA_USE_LINUX -Os"
@@ -14,6 +16,11 @@ clean:
 	$(RM) -f $(OBJS)
 	$(MAKE) -C $(LUA) clean
 	$(RM) -f $(TARGET_DIR)/xupnpd
+
+install: all
+	mkdir -p $(DESTDIR)$(PREFIX)/share/xupnpd
+	cp -pr *.lua {playlists,plugins,profiles,ui,www} $(DESTDIR)$(PREFIX)/share/xupnpd
+	$(INSTALL) -m 0755 -s $(TARGET_DIR)/xupnpd $(DESTDIR)$(PREFIX)/bin/xupnpd
 
 .c.o:
 	$(CC) -c $(EXTRA_CFLAGS) -o $@ $<
